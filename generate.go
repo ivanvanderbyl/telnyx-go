48de0c7370de8c4bc78cf4e977a1c68a4f502fb2
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2024-05-25 20:17:39 +0900

    Basic client generation
    Signed-off-by: Ivan Vanderbyl <ivanvanderbyl@gmail.com>

---
+++ generate.go
@@ -0,0 +1,4 @@
+package telnyx
+
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config gen-client.yaml -o client.gen.go slimspec.yaml
+// go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config gen-models.yaml -o models.gen.go slimspec.yaml
